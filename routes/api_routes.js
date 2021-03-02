52328c9d8157af409ac248c9f1181d9336af669c
David Bleiweiss <dbleiweiss123@gmail.com>
2021-03-02 15:20:00 -0500

    mongo stuff 3

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 let db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find().then(dbWorkout => {
+    db.Workout.find({}).then(dbWorkout => {
         res.json(dbWorkout);
     })
         .catch(err => {
